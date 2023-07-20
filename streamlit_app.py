718dc9305ed5d54af1b5b628e2b8d0073a417546
roychen092 <83868347+roychen092@users.noreply.github.com>
2023-07-20 16:04:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,11 @@
-import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
 
-def main():
-    st.title("Simple Streamlit App")
-    st.write("Welcome to this Streamlit app!")
-
-    # Add some interactive elements
-    name = st.text_input("Enter your name:", "Your Name")
-    age = st.slider("Select your age:", 1, 100, 25)
-
-    # Display the user's input
-    st.write(f"Hello, {name}! You are {age} years old.")
-
-    # Display an image
-    st.image("https://www.streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png",
-             caption="Streamlit Logo", use_column_width=True)
-
-    # Add a chart
-    chart_data = {'x': [1, 2, 3, 4, 5],
-                  'y': [10, 20, 30, 40, 50]}
-    st.line_chart(chart_data)
-
-if __name__ == "__main__":
-    main()
+# 1. as sidebar menu
+with st.sidebar:
+    selected = option_menu("Product", ["BFR CORPORATE", 'BFR mikro', 'BFR Consumer', 'BRF'], 
+        icons=['play', 'play'], menu_icon="cast", default_index=1)
+    selected
+    print(selected)
+df = pd.read_excel("adult.csv")
+st.dataframe(df)
